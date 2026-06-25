013d90c1b5e1d75881a566355502a0bfe3d07c67
Om Sai <omsai42467@gmail.com>
2026-06-25 20:29:54 +0530

    Added booking and payment management to backend

--- resort/admin.py
+++ resort/admin.py
@@ -2,6 +2,7 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for, current_app
 )
 from werkzeug.exceptions import abort
+import datetime
 
 from resort.auth import admin_required
 from resort.db import query_all, query_one, execute
@@ -14,7 +15,25 @@ bp = Blueprint('admin', __name__, url_prefix='/admin')
 @admin_required
 def dashboard():
     applicants = query_all('SELECT * FROM applications WHERE accepted IS NULL')
-    return render_template("admin/dashboard.html", applicants=applicants)
+    bookings = query_all('SELECT * FROM bookings')
+    return render_template("admin/dashboard.html", applicants=applicants, bookings=bookings, datetime=datetime)
+
+
+@bp.route('/bookings/<int:booking_id>/checkin')
+@admin_required
+def checkin(booking_id):
+    execute(f'UPDATE bookings SET ischeckedin = TRUE WHERE bid = {booking_id}')
+    flash('Guest checked in successfully!', 'success')
+    return redirect(url_for('admin.dashboard'))
+
+
+@bp.route('/bookings/<int:booking_id>/checkout')
+@admin_required
+def checkout(booking_id):
+    execute(f'UPDATE bookings SET ischeckedin = FALSE, enddate = CURRENT_DATE WHERE bid = {booking_id}')
+    flash('Guest checked out successfully!', 'success')
+    return redirect(url_for('admin.dashboard'))
+
 
 @bp.route('/applicants/<int:applicant_id>/accept')
 @admin_required
@@ -23,9 +42,11 @@ def acceptapp(applicant_id):
     flash('Applicant accepted successfully!', 'success')
     return redirect(url_for('admin.dashboard'))
 
+
 @bp.route('/applicants/<int:applicant_id>/reject')
 @admin_required
 def rejectapp(applicant_id):
     execute(f'UPDATE applications SET accepted = FALSE WHERE id = {applicant_id}')
     flash('Applicant rejected successfully!', 'success')
     return redirect(url_for('admin.dashboard'))
+
