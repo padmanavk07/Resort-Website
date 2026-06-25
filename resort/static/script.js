d88b7822423794ac268a8addebd6947c323757af
padmanavk07 <padmanav.k.07@outlook.com>
2026-06-25 17:51:23 +0530

    Added Admin Dashboard Page

--- resort/static/script.js
+++ resort/static/script.js
@@ -39,3 +39,37 @@ function hideSidebar() {
     const sidebar = document.querySelector('.side-bar');
     sidebar.style.right = "-300px";
 }
+
+// For admin page
+
+// Tabs changing
+const navItems = document.querySelectorAll('.admin-nav-item');
+const sections = document.querySelectorAll('.dashboard-section');
+const headerTitle = document.getElementById('header-title-text');
+
+navItems.forEach(item => {
+    item.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        navItems.forEach(nav => nav.classList.remove('active'));
+        sections.forEach(sec => sec.classList.remove('active'));
+        this.classList.add('active');
+        
+        const targetId = this.getAttribute('data-target');
+        document.getElementById(targetId).classList.add('active');
+        headerTitle.innerText = this.getAttribute('data-title');
+    });
+});
+
+// Check-in Button 
+function checkInGuest(id) {
+    const button = document.getElementById(`btn-${id}`);
+    const statusBadge = document.getElementById(`status-${id}`);
+    
+    statusBadge.className = "badge green";
+    statusBadge.innerText = "Checked In";
+    button.innerText = "Checked In";
+    button.classList.add("disabled");
+    button.disabled = true;
+    
+}
